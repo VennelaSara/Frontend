e22f4a7fab2d7a45f6dfdfc1a7f9b3b6a668cb01
VennelaSara <168427464+VennelaSara@users.noreply.github.com>
2025-04-15 12:43:59 +0530

    Update MatchingDisplay.jsx

--- MatchingDisplay.jsx
+++ MatchingDisplay.jsx
@@ -7,7 +7,7 @@ const MatchingDisplay = () => {
 
     useEffect(() => {
         setLoading(true);
-        fetch('http://localhost:5173/api/matches/')
+        fetch('https://mybackendresculpt-production.up.railway.app/api/matches/accept/')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch matches.');
@@ -25,7 +25,7 @@ const MatchingDisplay = () => {
 
     const handleAccept = async (matchId) => { // Added type annotation here
         try {
-            const response = await fetch(`http://localhost:5173/api/matches/accept/${matchId}`, {
+            const response = await fetch(`/api/matches/accept/api/matches/accept/${matchId}`, {
                 method: 'PUT',
             });
             if (!response.ok) {
@@ -47,7 +47,7 @@ const MatchingDisplay = () => {
 
     const handleReject = async (matchId) => { // Added type annotation here
         try {
-            const response = await fetch(`http://localhost:5173/api/matches/reject/${matchId}`, {
+            const response = await fetch(`/api/matches/accept/api/matches/reject/${matchId}`, {
                 method: 'PUT',
             });
             if (!response.ok) {
