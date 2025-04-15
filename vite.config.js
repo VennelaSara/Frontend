3d48d7ff383e17653553bd6b76bc6078f5efebd3
VennelaSara <168427464+VennelaSara@users.noreply.github.com>
2025-04-15 11:46:10 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  publicDir: 'public', // Explicitly set the public directory
+  build: {
+    outDir: 'dist', // Make sure your output directory is 'dist'
+  },
+})
